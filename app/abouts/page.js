a7ca4e02e34abbd21429772967a99b6e0680d0aa
raksmeymac <raksmey.itmoh@gmail.com>
2024-07-29 22:48:03 +0700

    update

--- app/abouts/page.js
+++ app/abouts/page.js
@@ -3,8 +3,12 @@ import React from "react";
 const page = () => {
   return (
     <div className=" flex justify-center  py-8">
-      <div className=" w-[600px]  bg-slate-200 p-6 rounded-sm  ">
+      <div className=" w-[600px] bg-slate-50 p-6 rounded-sm  ">
         <h5 className=" mt-1">About me!</h5>
+        <h4>
+          "Raksmey CHann" is a Full Stack Web Developer !
+          <br />
+        </h4>
       </div>
     </div>
   );
