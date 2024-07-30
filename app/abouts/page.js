a6416b50220add7cfe2035d1f24b8cc4104d5581
raksmeymac <raksmey.itmoh@gmail.com>
2024-07-30 08:09:24 +0700

    update 30-07

--- app/abouts/page.js
+++ app/abouts/page.js
@@ -5,13 +5,8 @@ const page = () => {
     <div className=" flex justify-center  py-8">
       <div className=" w-[600px] bg-slate-50 p-6 rounded-sm  ">
         <h5 className=" mt-1">About me!</h5>
-        <h4>
-          "Raksmey CHann" is a Full Stack Web Developer !
-          <br />
-          "Contactt me" at raksmey.itmoh@gmail.com"
-          <br />
-          "Telegram me" at 0885290240
-        </h4>
+        <h4>Raksmey-Chann is a Full Stack Web Developer !</h4>
+        <h4>Contact Me : at raksmey.itmoh@gmail.com / 0885290240</h4>
       </div>
     </div>
   );
