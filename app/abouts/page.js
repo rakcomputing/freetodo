f733fbc1ed08b76c9cc807207712782458536346
raksmeymac <raksmey.itmoh@gmail.com>
2024-07-30 08:17:01 +0700

    update 30-0-7

--- app/abouts/page.js
+++ app/abouts/page.js
@@ -1,12 +1,11 @@
 import React from "react";
-
 const page = () => {
   return (
     <div className=" flex justify-center  py-8">
       <div className=" w-[600px] bg-slate-50 p-6 rounded-sm  ">
         <h5 className=" mt-1">About me!</h5>
         <h4>Raksmey-Chann is a Full Stack Web Developer !</h4>
-        <h4>Contact Me : at raksmey.itmoh@gmail.com / 0885290240</h4>
+        <h4>Contact Me : at raksmey.itmoh@gmail.com / 0885290240/089213339</h4>
       </div>
     </div>
   );
