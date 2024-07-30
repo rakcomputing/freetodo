b5235f3db74bb3be3d9749d07295808d6cb21f86
raksmeymac <raksmey.itmoh@gmail.com>
2024-07-30 07:58:24 +0700

    update 30-07

--- app/login/page.js
+++ app/login/page.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className=" flex justify-center  py-8">
+      <div className=" w-[600px] bg-slate-50 p-6 rounded-sm  ">
+        <h5 className=" mt-1">Account Setting</h5>
+      </div>
     </div>
   );
 };
